084500fcf59a7791532697c8af85099d8d75b92c
pjjajal <jajalpurvish@gmail.com>
2024-11-07 21:04:59 -0500

    cleaned up 21k and added in1k. no more streaming (for now).

--- dl_datasets/imagenetw21.py
+++ dl_datasets/imagenetw21.py
@@ -1,32 +1,16 @@
 from datasets import load_dataset
 from functools import partial
+from .utils import process_data
 
 TOTAL_SAMPLES = 13_151_276
+TOTAL_VAL_SAMPLES = None  # no validation set
 TOTAL_CLASSES = 19_167
 URL = "timm/imagenet-w21-wds"
 KEYS = {"image": "jpg", "label": "cls"}
 
 
-def process_data(sample, transform=None):
-    # sample["cls"] = [label for label in sample["cls"]] # subtract 1 to make it 0-indexed
-    if transform is not None:
-        sample["jpg"] = [transform(img) for img in sample["jpg"]]
-    return sample
-
-def imagenetw21_train(
-    transform=None,
-    seed=245,
-    shuffle_buffer_size=1_000,
-    streaming=True,
-    num_proc=1,
-):
-    dataset = load_dataset(URL, split="train", streaming=streaming, num_proc=num_proc)
-    # dataset = dataset.map(
-    #     map_data,
-    #     batched=True,
-    #     remove_columns=dataset.column_names,
-    #     num_proc=num_proc
-    # )
-    dataset = dataset.select_columns(['jpg', 'cls']).with_format("torch")
+def imagenetw21_train(transform=None, num_proc=1):
+    dataset = load_dataset(URL, split="train", num_proc=num_proc)
+    dataset = dataset.select_columns(["jpg", "cls"]).with_format("torch")
     dataset.set_transform(partial(process_data, transform=transform))
     return dataset
--- dl_datasets/utils.py
+++ dl_datasets/utils.py
@@ -3,3 +3,8 @@ from torch.utils.data import default_collate
 def image_collate_fn(batch):
     batch = default_collate(batch)
     return batch['jpg'], batch['cls']
+
+def process_data(sample, transform=None):
+    if transform is not None:
+        sample["jpg"] = [transform(img) for img in sample["jpg"]]
+    return sample
